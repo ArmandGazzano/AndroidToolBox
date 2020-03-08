c73095c1d3f06946604c2f8129c55749ce1bea34
ArmandGazzano <gazzano90@gmail.com>
2020-03-08 21:17:48 +0100

    Ajout de l'interface qui au clique permettra à l’utilisateur d’accéder et de récupérer une photo de la galerie ou d'en prendre une depuis la camera. Cette photo remplacera l’image dans l’interface.

--- app/src/main/java/fr/isen/gazzano/androidtoolbox/PermissionsActivity.kt
+++ app/src/main/java/fr/isen/gazzano/androidtoolbox/PermissionsActivity.kt
@@ -1,14 +1,25 @@
 package fr.isen.gazzano.androidtoolbox
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.ContentResolver
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Bitmap
+import android.icu.util.Calendar
+import android.media.MediaScannerConnection
 import android.os.Bundle
+import android.os.Environment
 import android.provider.ContactsContract
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_permissions.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 
 class PermissionsActivity : AppCompatActivity() {
@@ -20,11 +31,29 @@ class PermissionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_permissions)
 
         getContacts()
-        contact_list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contacts.sorted())
+        contact_list.adapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_1, contacts.sorted())
 
-        imageView.setOnClickListener{
-            pickImageFromGallery()
+        imageView.setOnClickListener {
+            showPictureDialog();
         }
+    }
+
+    private fun showPictureDialog() {
+        val pictureDialog: AlertDialog.Builder = AlertDialog.Builder(this)
+        pictureDialog.setTitle("Select Action")
+        val pictureDialogItems = arrayOf(
+            "Select photo from gallery",
+            "Capture photo from camera"
+        )
+        pictureDialog.setItems(pictureDialogItems,
+            DialogInterface.OnClickListener { dialog, which ->
+                when (which) {
+                    0 -> pickImageFromGallery()
+                    1 -> takePictureIntent()
+                }
+            })
+        pictureDialog.show()
     }
 
     private fun pickImageFromGallery() {
@@ -33,9 +62,19 @@ class PermissionsActivity : AppCompatActivity() {
         startActivityForResult(intent, 1000)
     }
 
+    private fun takePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, 1001)
+            }
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK && requestCode == 1000){
+        if (resultCode == Activity.RESULT_OK && requestCode == 1000) {
             imageView.setImageURI(data?.data)
+        }else if (resultCode == Activity.RESULT_OK && requestCode == 1001) {
+            takePictureIntent()
         }
     }
 
